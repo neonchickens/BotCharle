328a53fd1b488fc30289f5588414589d562ee50d
Weston Laity <wlaity517@gmail.com>
2018-10-27 22:37:05 -0400

    Bot comes online.

---
+++ src/io/neonchickens/github/botcharle/BotCharle.java
@@ -0,0 +1,20 @@
+package io.neonchickens.github.botcharle;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class BotCharle extends ListenerAdapter {
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent evMessage) {
+		
+
+		
+	}
+	
+}
