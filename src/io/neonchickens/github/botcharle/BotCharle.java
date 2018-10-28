63ee2c42b76f054b067d27ec96ce6244121d24fe
Weston Laity <wlaity517@gmail.com>
2018-10-27 23:19:47 -0400

    Started setup for message response.

--- src/io/neonchickens/github/botcharle/BotCharle.java
+++ src/io/neonchickens/github/botcharle/BotCharle.java
@@ -1,20 +1,24 @@
 package io.neonchickens.github.botcharle;
 
-import javax.security.auth.login.LoginException;
-
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class BotCharle extends ListenerAdapter {
 	
 	@Override
-	public void onMessageReceived(MessageReceivedEvent evMessage) {
-		
+	public void onMessageReceived(MessageReceivedEvent eventMessage) {
 
+		User msgAuthor = eventMessage.getAuthor();
+		
+		if (msgAuthor.isBot()) {
+
+			Guild msgGuild = eventMessage.getGuild();
+			MessageChannel msgMessageChannel = eventMessage.getChannel();
+			
+		}
 		
 	}
-	
 }
